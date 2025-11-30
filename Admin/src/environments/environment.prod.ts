e4c762776928772f1c49d6642a8295f594201c1f
Oscar Valdivieso <fernandoscar04@gmail.com>
2025-11-29 22:46:27 -0600

    fix env

--- Admin/src/environments/environment.prod.ts
+++ Admin/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   defaultauth: 'fakebackend',
-  apiUrl: 'https://sistema-viajes-api.onrender.com/',
+  apiUrl: 'https://sistema-viajes-api.onrender.com',
   //apiUrl: 'https://localhost:7228',
   apiKey: '4b567cb1c6b24b51ab55248f8e66e5cc',
 
